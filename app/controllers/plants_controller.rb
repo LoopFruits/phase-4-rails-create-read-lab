a2726fcda64105253467f05dc596792d3f0c37f9
LoopFruits <Kensworldc@gmail.com>
2022-04-06 15:48:58 -0400

    Done.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,12 +1,30 @@
 class PlantsController < ApplicationController
+  #Get /plants
   def index
+    #returns an array of all plants 
     plants = Plant.all
     render json: plants 
   end
 
+  #Get /plants/:id
   def show
+    #returns the first and second plant 
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
   end
 
+  #post /plants
   def create
+    #creates new plant 
+    plant = Plant.create(params[:plant])
+    #returns the plant data 
+    render json: plant, status: :created 
+    #returns a status code of 201 
+  end
+
+  private 
+
+  def plant_params 
+    params.require(:plant).permit(:name, :image, :price)
   end
 end
